4bfb1cf300d9133e7e7689395ea5082705458c67
Julien DAUPHANT <julien.dauphant@beta.gouv.fr>
2022-11-27 02:44:14 +0100

    Simplify tray launcher

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,18 +7,17 @@ use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent, SystemT
 use tauri::Manager;
 use tauri::api;
 
-const HIDE_TEXT: &str = "Masquer / Hide";
-const SHOW_TEXT: &str = "Afficher / Show";
+const SHOW_TEXT: &str = "Afficher / Show Tchap";
 const QUIT_TEXT: &str = "Quitter / Quit";
-const HIDE_SHOW_ITEM_ID: &str = "hide_show";
+const SHOW_ITEM_ID: &str = "show";
 const FAQ_ITEM_ID: &str = "faq";
 
 fn main() {
-    let hide_show = CustomMenuItem::new(HIDE_SHOW_ITEM_ID.to_string(), HIDE_TEXT);
+    let show = CustomMenuItem::new(SHOW_ITEM_ID.to_string(), SHOW_TEXT);
     let quit = CustomMenuItem::new("quit".to_string(), QUIT_TEXT);
     let faq = CustomMenuItem::new(FAQ_ITEM_ID.to_string(), "FAQ");
     let tray_menu = SystemTrayMenu::new()
-        .add_item(hide_show)
+        .add_item(show)
         .add_item(faq)
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(quit);
@@ -28,24 +27,16 @@ fn main() {
         .system_tray(system_tray)
         .on_system_tray_event(|app, event| match event {
             SystemTrayEvent::MenuItemClick { id, .. } => {
-                let item_handle = app.tray_handle().get_item(&id);
                 let window = app.get_window("main").unwrap();
                 match id.as_str() {
                     "quit" => {
                         std::process::exit(0);
                     }
-                    HIDE_SHOW_ITEM_ID => {
-                        match window.is_visible().unwrap() {
-                            false => {
-                                window.show().unwrap();
-                                window.set_focus().unwrap();
-                                item_handle.set_title(HIDE_TEXT).unwrap();
-                            }
-                            _ => {
-                                window.hide().unwrap();
-                                item_handle.set_title(SHOW_TEXT).unwrap();
-                            }
-                        }
+                    SHOW_ITEM_ID => {
+                       if !window.is_visible() {
+                            window.show().unwrap();
+                       }
+                       window.set_focus().unwrap();
                     }
                     FAQ_ITEM_ID => {
                         api::shell::open(&window.shell_scope(), "https://tchap.gouv.fr/faq".to_string(), None).unwrap();
